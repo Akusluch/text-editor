d06735994c5e83a7d51dc8481859267af6d26ec3
Alphonse Kusluch <alkusluch@gmail.com>
2024-02-14 20:04:18 -0500

    update webpack config to create manifest and service worker

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,40 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ id: 1, value: content});
+
+  const result = await request;
+  console.log('data saved', result);
+
+  // console.error('putDb not implemented');
+}
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log(result)
+
+  return result.value
+  // console.error('getDb not implemented');
+}
 
 initdb();
