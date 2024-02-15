f702b5ea9bf120d8c52c687f77da76296cbaf043
Alphonse Kusluch <alkusluch@gmail.com>
2024-02-14 21:11:17 -0500

    add data caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -26,5 +26,23 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+
+registerRoute(
+  // callback function to filter the request for js and css
+  ({request}) => ['style', 'script', 'worker'].includes(request.destination),
+
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins:[
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        
+        maxEntries: 60,
+        // 30 days
+        maxAgeSeconds: 30*24*60*60
+      })
+    ]
+  })
+);
